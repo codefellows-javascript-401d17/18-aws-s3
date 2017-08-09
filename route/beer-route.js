9c5fae73190b2a1837346fa307cfdc7723af2375
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-09 15:14:55 -0700

    rewrote beer routes

--- route/beer-route.js
+++ route/beer-route.js
@@ -1,51 +1,70 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const del = require('del');
+const AWS = require('aws-sdk');
+const multer = require('multer');
 const Router = require('express').Router;
-const jsonParser = require('body-parser').json();
-const debug = require('debug')('brewery:beer router');
-const Brewery = require('../model/brewery.js');
+const createError = require('http-errors');
+const debug = require('debug')('brewery:beer-route');
+
 const Beer = require('../model/beer.js');
+const Brewery = require('../model/brewery.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
-const beerRouter = module.exports = new Router();
+AWS.config.setPromisesDependency(require('bluebird'));
 
-beerRouter.post('/api/brewery/:breweryID/beer', jsonParser, function(req, res, next){
+const s3 = new AWS.S3();
+const datadir = `${__dirname}/../data`;
+const upload = multer({ dest: datadir });
+
+const beerRouter = module.exports = Router();
+
+function s3uploadProm(params) {
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, s3data) => {
+      resolve(s3data);
+    });
+  });
+}
+
+beerRouter.post('/api/brewery/:breweryID/beer', bearerAuth, upload.single('image'), function(req, res, next){
   debug('POST: /api/brewery/:breweryID/beer');
 
-  Brewery.findByIdAndAddBeer(req.params.breweryID, req.body)
-  .then( beer => res.json(beer))
-  .catch(next);
-});
-
-beerRouter.get('/api/brewery/:breweryID/beer/:id', function(req, res, next){
-  debug('GET: /api/brewery/:breweryID/beer/:id');
-
-  Beer.findById(req.params.id)
-  .then(beer => res.json(beer))
-  .catch(next);
-});
-
-beerRouter.put('/api/brewery/:breweryID/beer/:id',jsonParser , function(req, res, next){
-  debug('PUT: /api/brewery/:breweryID/beer/:id');
-
-  if(Object.keys(req.body).length === 0) {
-    Beer.findById(req.params.id)
-    .then(beer => {
-      res.status(400);
-      res.json(beer);
-    })
-    .catch(next);
-    return;
+  if(!req.file){
+    return next(createError(400, 'file not found'));
   }
 
-  Beer.findByIdAndUpdate(req.params.id, req.body, {new: true} )
-  .then(beer => res.json(beer))
-  .catch(next);
-});
+  if(!req.file.path){
+    return next(createError(400, 'file not saved'));
+  }
 
-beerRouter.delete('/api/brewery/:breweryID/beer/:id', function (req, res , next){
-  debug('DELETE: /api/brewery/:breweryID/beer/:id');
+  let ext = path.extname(req.file.originalname);
 
-  Beer.findByIdAndRemove(req.params.id)
-  .then(() => res.status(204).send())
-  .catch(next);
+  let params = {
+    ACL: 'public-read',
+    Bucket: process.end.AWS_BUCKET,
+    Key: `${req.file.filename}${ext}`,
+    Body: fs.createReadStream(req.file.path),
+  };
+
+  Brewery.findById(req.params.breweryID)
+  .then( () => s3uploadProm(params))
+  .then( s3data => {
+    console.log('s3data', s3data);
+    del([`${datadir}/*`]);
+    let beerData = {
+      name: req.body.name,
+      style: req.body.style,
+      ibu: req.body.ibu,
+      objectKey: s3data.key,
+      imageURI: s3data.Location,
+      userID: req.user._id,
+      breweryID: req.params.breweryID,
+    };
+    return new Beer(beerData).save();
+  })
+  .then( beer =>res.json(beer))
+  .catch( err => next(err));
 });
