c5a10dc73e61e186e784a1c1c5a3d39cd0e73f49
andyfiveeleven <fouracesguitar@gmail.com>
2017-08-09 14:44:56 -0700

    tests don't work because i don't have aws set up (aws's fualt not mine), hopefully they work on your computer

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 
 const authRouter = require('./route/auth-router.js');
 const galleryRouter = require('./route/gallery-router.js');
+const picRouter = require('./route/pic-router.js');
 const errors = require('./lib/error-middleware.js');
 
 dotenv.load();
@@ -22,6 +23,7 @@ app.use(morgan('dev'));
 
 app.use(authRouter);
 app.use(galleryRouter);
+app.use(picRouter);
 app.use(errors);
 
 app.listen(PORT, () =>  {
