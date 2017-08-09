1ff215e3d3f9759e89e68e5b69d63817044c187e
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-09 14:42:50 -0700

    updated beer model

--- model/beer.js
+++ model/beer.js
@@ -7,7 +7,11 @@ const beerSchema = Schema ({
   name: {type: String, required: true},
   style: {type: String, require: true},
   ibu: {type: String, required: true},
-  breweryID: {type: Schema.Types.ObjectId, required: true }
+  userID: {type: Schema.types.ObjectId, require: true},
+  breweryID: {type: Schema.Types.ObjectId, required: true },
+  imageURI: {type: String, required: true, unique: true},
+  objectKey: {type: String, required: true, unique: true},
+  created: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('beer', beerSchema);
