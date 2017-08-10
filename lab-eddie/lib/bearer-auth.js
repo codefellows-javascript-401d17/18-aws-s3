52570d15e11d53285bd1d41376d48ac4ff6774a3
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-09 18:25:04 -0700

    Wrote bearer auth middle ware

--- lab-eddie/lib/bearer-auth.js
+++ lab-eddie/lib/bearer-auth.js
@@ -2,7 +2,26 @@
 
 const createError = require('http-errors');
 const debug = require('debug')('app: bear-auth-middleware');
+const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
   debug('bear- auth')
+
+  let authHeader = req.headers.authorization;
+
+  if(!authHeader) return next(createError(401, 'Authorization Header Required'));
+
+  let splitHead = authHeader.split('Bearer ')[1];
+  if(!splitHead) return next(createError(401, 'Token required'));
+
+  jwt.verify(splitHead, process.env.APP_SECRET, (err, decde) => {
+    if(err) return next(createError(401, err.message));
+
+    User.findOne({hash: decode.token})
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => next(createError(401, err.message)));
+  });
 };
